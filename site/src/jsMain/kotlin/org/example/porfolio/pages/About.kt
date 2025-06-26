e0bd63f980a6f31c38f7f54fe50bca4dbf5699c7
goransipic <goran.sipic@certilia.com>
2025-06-26 22:55:25 +0200

    about change #2

--- site/src/jsMain/kotlin/org/example/porfolio/pages/About.kt
+++ site/src/jsMain/kotlin/org/example/porfolio/pages/About.kt
@@ -4,19 +4,36 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
+import com.varabyte.kobweb.compose.css.FontWeight
 import com.varabyte.kobweb.compose.css.functions.LinearGradient
 import com.varabyte.kobweb.compose.css.functions.linearGradient
 import com.varabyte.kobweb.compose.foundation.layout.Box
+import com.varabyte.kobweb.compose.foundation.layout.Column
 import com.varabyte.kobweb.compose.ui.Alignment
 import com.varabyte.kobweb.compose.ui.Modifier
+import com.varabyte.kobweb.compose.ui.graphics.Colors
+import com.varabyte.kobweb.compose.ui.modifiers.backgroundColor
 import com.varabyte.kobweb.compose.ui.modifiers.backgroundImage
+import com.varabyte.kobweb.compose.ui.modifiers.color
 import com.varabyte.kobweb.compose.ui.modifiers.fillMaxSize
+import com.varabyte.kobweb.compose.ui.modifiers.fillMaxWidth
+import com.varabyte.kobweb.compose.ui.modifiers.fontSize
+import com.varabyte.kobweb.compose.ui.modifiers.fontWeight
+import com.varabyte.kobweb.compose.ui.modifiers.height
+import com.varabyte.kobweb.compose.ui.modifiers.margin
+import com.varabyte.kobweb.compose.ui.modifiers.padding
+import com.varabyte.kobweb.compose.ui.modifiers.size
 import com.varabyte.kobweb.core.Page
+import com.varabyte.kobweb.silk.components.graphics.Image
+import com.varabyte.kobweb.silk.components.text.SpanText
 import com.varabyte.kobweb.silk.theme.colors.ColorMode
+import com.varabyte.kobweb.silk.theme.colors.palette.color
+import com.varabyte.kobweb.silk.theme.colors.palette.toPalette
 import kotlinx.browser.localStorage
 import org.example.porfolio.components.ProfileCard
 import org.example.porfolio.components.ThemeSwitchButton
 import org.example.porfolio.util.Res
+import org.jetbrains.compose.web.css.px
 import org.jetbrains.compose.web.dom.Text
 
 @Page
@@ -38,6 +55,22 @@ fun AboutPage() {
             ),
         contentAlignment = Alignment.Center
     ) {
-        Text("Work in progress")
+        Box(
+            Modifier.fillMaxWidth()
+                .backgroundColor(Res.Theme.GRADIENT_ONE.color)
+                .fontSize(30.px)
+                .color(colorMode.toPalette().color)
+                .height(70.px)
+                .align(
+                    Alignment.TopCenter
+                ),
+            contentAlignment = Alignment.Center
+        ) {
+            SpanText("Header")
+        }
+        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+            SpanText("Work in progress", Modifier.fontSize(40.px).fontWeight(FontWeight.Bold).margin(bottom = 30.px))
+            Image("/image_split.jpg")
+        }
     }
 }
